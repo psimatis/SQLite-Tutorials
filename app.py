b8af8849757694556474c1cbed81ab840d0de79c
psimatis <psimatis@connect.ust.hk>
2023-03-02 15:34:34 +0800

    list as in-memory database, added prompt

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from database import add_entry, view_entries
+from database import add_entry, get_entries
 
 menu = """Please select:
 1) Add new entry for today
@@ -10,10 +10,22 @@ Your selection:"""
 welcome = "Welcome to the programming diary"
 print(welcome)
 
+def prompt_new_entry():
+    entry_content = input('What have your learned today?')
+    entry_date = input('Enter the date:')
+
+    add_entry(entry_content, entry_date)
+
+
+def view_entries(entries):
+    for entry in entries:
+        print(f"{entry['date']}\n{entry['content']}\n\n")
+
+
 while (user_input := input(menu)) != '3':
     if user_input == '1':
-        add_entry()
+        prompt_new_entry()
     elif user_input == '2':
-        view_entries()
+        view_entries(get_entries())
     else:
         print('Invalid option')
