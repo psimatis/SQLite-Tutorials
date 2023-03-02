89a95039e66f6d0f4f60a834bb74c1a389c7ab49
psimatis <psimatis@connect.ust.hk>
2023-03-02 16:48:39 +0800

    added db query, and converted the cursor results to dict

--- database.py
+++ database.py
@@ -2,14 +2,18 @@ import sqlite3
 
 connection = sqlite3.connect('data.db')
 
+connection.row_factory = sqlite3.Row # by default sqlite returns tuples, this returns dict. a little slower
+
 def create_table():
     with connection:
         connection.execute("CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT);")
 
+
 def add_entry(entry_content, entry_date):
-    with connection:
+    with connection: # needed when make changes to the DB
         connection.execute(f"INSERT INTO entries VALUES (?, ?);", (entry_content, entry_date))
 
 
 def get_entries():
-    return entries
+    cursor = connection.execute("SELECT * FROM entries;") # cursor points at the first row
+    return cursor
