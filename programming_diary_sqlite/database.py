c2762ee7abe4a089442bfbaf687b98ffce442ca0
psimatis <psimatis@connect.ust.hk>
2023-03-03 10:14:07 +0800

    added movies db, includes update

--- programming_diary_sqlite/database.py
+++ programming_diary_sqlite/database.py
@@ -1,8 +1,8 @@
 import sqlite3
 
 connection = sqlite3.connect('data.db')
+connection.row_factory = sqlite3.Row  # by default sqlite returns tuples, this returns dict. a little slower
 
-connection.row_factory = sqlite3.Row # by default sqlite returns tuples, this returns dict. a little slower
 
 def create_table():
     with connection:
@@ -10,10 +10,10 @@ def create_table():
 
 
 def add_entry(entry_content, entry_date):
-    with connection: # needed when make changes to the DB
+    with connection:  # needed when make changes to the DB
         connection.execute(f"INSERT INTO entries VALUES (?, ?);", (entry_content, entry_date))
 
 
 def get_entries():
-    cursor = connection.execute("SELECT * FROM entries;") # cursor points at the first row
+    cursor = connection.execute("SELECT * FROM entries;")  # cursor points at the first row
     return cursor
