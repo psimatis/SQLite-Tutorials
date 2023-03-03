89a8579ec9d9d4352fb90b21a7a07ecc7c4f57c4
psimatis <psimatis@connect.ust.hk>
2023-03-03 16:32:47 +0800

    broke down to multiple tables

--- movies_watchlist/app.py
+++ movies_watchlist/app.py
@@ -31,17 +31,23 @@ def print_movie_list(heading, movies):
     for movie in movies:
         movie_date = datetime.datetime.fromtimestamp(movie[1])
         human_date = movie_date.strftime("%d %b %Y")
-        print(f"{movie[0]} (on {human_date}")
+        print(f"{movie[0]} on {human_date}")
     print('---\n')
 
 
+def print_watched_movie_list(heading, movies):
+    print(f'{username} watched movies')
+    for movie in movies:
+        print(f"{movie[1]}")
+    print('---\n')
+
 def prompt_watch_movie():
+    username = input('Username:')
     movie_title = input("Enter movie title you watched:")
-    database.watch_movie(movie_title)
+    database.watch_movie(username, movie_title)
 
 
 while (user_input := input(menu)) != "6":
-    print(type(user_input))
     if user_input == "1":
         prompt_add_movie()
     elif user_input == "2":
@@ -53,7 +59,8 @@ while (user_input := input(menu)) != "6":
     elif user_input == "4":
         prompt_watch_movie()
     elif user_input == "5":
-        movies = database.get_watched_movies()
-        print_movie_list("Watched", movies)
+        username = input('Username: ')
+        movies = database.get_watched_movies(username)
+        print_watched_movie_list(username, movies)
     else:
         print("Invalid input, please try again!")
